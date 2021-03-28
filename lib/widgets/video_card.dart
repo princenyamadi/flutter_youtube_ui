b1c255b2c1de4002cf4466ef9cd703fade3a5fba
princenyamadi <princenyamadi.pn@gmail.com>
2021-03-28 12:43:59 +0000

    feat: added video card widget

---
+++ lib/widgets/video_card.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_youtube_ui/data.dart';
+import 'package:flutter_youtube_ui/screens/nav_screen.dart';
+import 'package:miniplayer/miniplayer.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+class VideoCard extends StatelessWidget {
+  final Video video;
+
+  VideoCard({Key? key, required this.video}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.read(selectedVideoProvider).state = video;
+        context
+            .read(miniPlayerControllerProvider)
+            .state
+            .animateToHeight(state: PanelState.MAX);
+      },
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Image.network(
+                video.thumbnailUrl,
+                height: 220.0,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              Positioned(
+                bottom: 8.0,
+                right: 8.0,
+                child: Container(
+                  color: Colors.black,
+                  child: Text(
+                    video.duration,
+                    style: Theme.of(context)
+                        .textTheme
+                        .caption!
+                        .copyWith(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () => print('Navigate to profile'),
+                  child: CircleAvatar(
+                    foregroundImage: NetworkImage(video.author.profileImageUrl),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8.0,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          video.title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontSize: 15.0),
+                        ),
+                      ),
+                      Flexible(
+                        child: Text(
+                          '${video.author.username} * ${video.viewCount} * ${timeago.format(video.timestamp)}',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontSize: 14.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.more_vert,
+                    size: 20.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
